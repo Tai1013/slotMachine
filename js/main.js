53b1b6dc781f9b4b5b02dec8943056f4324f946d
Tai Fu <frank22199215@gmail.com>
2022-05-03 01:13:00 +0800

    add record bet

--- js/main.js
+++ js/main.js
@@ -2,61 +2,87 @@ const app = new Vue({
     data() {
         return {
             bet: 10,
-            totalBet: 500,
+            totalBet: 1000.00,
             double: 1,
 
             onload: false,
-            resultData: [],
+            onRepeat: false,
             onSpeedUp: false,
+            resultData: [],
             speed: 15,
+            winLineTime: 2,
             reelWinData: [
                 {
                     tile: [1, 1, 1],
-                    bet: 10,
+                    bet: 5,
                     class: 'line456'
                 },
                 {
                     tile: [0, 0, 0],
-                    bet: 5,
+                    bet: 2,
                     class: 'line123'
                 },
                 {
                     tile: [2, 2, 2],
-                    bet: 5,
+                    bet: 2,
                     class: 'line789'
                 },
                 {
                     tile: [0, 1, 2],
-                    bet: 1,
+                    bet: 0.5,
                     class: 'line159'
                 },
                 {
                     tile: [2, 1, 0],
-                    bet: 1,
+                    bet: 0.5,
                     class: 'line357'
                 },
-            ]
+            ],
+
+            recordData: [],
+            currentID: 0,
+            currentDate: '',
+            currentTime: '',
+            currentReel: [],
+            currentWin: 0,
+            currentDetails: [],
         }
     },
     watch: {
         onSpeedUp() {
-            if (this.onSpeedUp) this.speed = this.speed * 2
-            else this.speed = this.speed / 2
+            if (this.onSpeedUp) {
+                this.speed = this.speed * 3
+                this.winLineTime = this.winLineTime / 2
+            }
+            else {
+                this.speed = this.speed / 3
+                this.winLineTime = this.winLineTime * 2
+            }
         }
     },
     methods: {
         reset() {
-            this.resultData.length = 0
-            this.onload = false
+            Object.assign(this.$data, this.$options.data());
         },
 
         start() {
+            const today = new Date()
             const min = 10
             const max = 30
             let momentum = Math.floor(Math.random() * (max - min + 1) + min);
             let maxTurns = []
 
+            function twoDigits(val) {
+                if (val < 10) return "0" + val;
+                return val;
+            }
+
+            this.currentWin = 0
+            this.currentID++
+            this.currentDate = today.getFullYear() + '-' + twoDigits(today.getMonth() + 1) + '-' + twoDigits(today.getDate())
+            this.currentTime = twoDigits(today.getHours()) + ":" + twoDigits(today.getMinutes()) + ":" + twoDigits(today.getSeconds())
             this.onload = !this.onload
+            this.totalBet = this.totalBet - this.bet * this.double
 
             for (let i = 0; i < 3; i++) {
                 const delayTurns = Math.floor(Math.random() * min + 1);
@@ -66,9 +92,9 @@ const app = new Vue({
                     maxTurns.push(momentum)
                 }
             }
-            this.$refs.reel1.run(maxTurns[0], this.onSpeedUp)
-            this.$refs.reel2.run(maxTurns[1], this.onSpeedUp)
-            this.$refs.reel3.run(maxTurns[2], this.onSpeedUp)
+            this.$refs.reel1.run(maxTurns[0])
+            this.$refs.reel2.run(maxTurns[1])
+            this.$refs.reel3.run(maxTurns[2])
         },
 
         reelStopped(data) {
@@ -77,13 +103,12 @@ const app = new Vue({
             if (this.resultData.length == reelTotal) {
                 this.checkResult(this.resultData)
             }
-
-
         },
 
         checkResult(result) {
             const doubleBet = this.bet * this.double    //倍率下注額
-            let winBet = 0
+            let resultData = []
+            this.currentReel = [result[0][0].name, result[1][0].name, result[2][0].name, result[0][1].name, result[1][1].name, result[2][1].name, result[0][2].name, result[1][2].name, result[2][2].name]
             this.reelWinData.forEach((data, index) => {
                 const r1 = result[0][data.tile[0]]
                 const r2 = result[1][data.tile[1]]
@@ -92,43 +117,71 @@ const app = new Vue({
                 const tile1Name = r1.name
                 const tile2Name = r2.name
                 const tile3Name = r3.name
-
                 const lineBet = doubleBet * data.bet
                 let tailBet = 0
 
                 if (tile1Name == tile2Name && tile2Name == tile3Name) tailBet = doubleBet * r2.bonus
-                if (tile2Name == 'scatter' && tile3Name == 'scatter' && tile1Name != 'seven') tailBet = doubleBet * r1.bonus
-                if (tile1Name == 'scatter' && tile3Name == 'scatter' && tile2Name != 'seven') tailBet = doubleBet * r2.bonus
-                if (tile1Name == 'scatter' && tile2Name == 'scatter' && tile3Name != 'seven') tailBet = doubleBet * r3.bonus
-                if (tile2Name == 'scatter' && tile1Name == tile3Name) tailBet = doubleBet * r1.bonus
-                if (tile1Name == 'scatter' && tile2Name == tile3Name) tailBet = doubleBet * r2.bonus
-                if (tile3Name == 'scatter' && tile1Name == tile2Name) tailBet = doubleBet * r2.bonus
+                if (tile2Name == 'scatter' && tile3Name == 'scatter' && tile1Name != 'seven') tailBet = doubleBet * r1.bonus / 4
+                if (tile1Name == 'scatter' && tile3Name == 'scatter' && tile2Name != 'seven') tailBet = doubleBet * r2.bonus / 4
+                if (tile1Name == 'scatter' && tile2Name == 'scatter' && tile3Name != 'seven') tailBet = doubleBet * r3.bonus / 4
+                if (tile2Name == 'scatter' && tile1Name == tile3Name) tailBet = doubleBet * r1.bonus / 2
+                if (tile1Name == 'scatter' && tile2Name == tile3Name) tailBet = doubleBet * r2.bonus / 2
+                if (tile3Name == 'scatter' && tile1Name == tile2Name) tailBet = doubleBet * r2.bonus / 2
 
                 if (tailBet > 0) {
                     console.log('tailBet:' + tailBet + ' 水果倍率:' + tailBet / doubleBet)
                     console.log('lineBet:' + lineBet + ' 線倍率:' + data.bet)
-                    this.animate(data.class)
-                    winBet += (lineBet + tailBet)
+                    resultData.push({ 
+                        class: data.class,
+                        winBet: lineBet + tailBet
+                    })
+                    this.currentDetails.push({ 
+                        tail: [tile1Name, tile2Name, tile3Name],
+                        winBet: lineBet + tailBet
+                    })
                 }
             })
 
-            if (winBet == 0) this.end()
+            if (resultData.length > 0) {
+                for (let i = 0; i < resultData.length; i++) {
+                    setTimeout(() => {
+                        this.animate(resultData[i].class, resultData[i].winBet, (i + 1) == resultData.length)
+                        this.totalBet += resultData[i].winBet
+                        this.currentWin += resultData[i].winBet
+                    }, 1000 * this.winLineTime * i)
+                }
+            } 
+            else this.end()
         },
 
         end() {
+            this.recordData.push({
+                id: 'slot777-' + this.currentID,
+                date: this.currentDate,
+                time: this.currentTime,
+                bet: this.bet,
+                double: this.double,
+                winBet: this.currentWin,
+                winLoss: this.currentWin < 0 ? false : true,
+                details: {
+                    reel: this.currentReel,
+                    win: this.currentDetails
+                },  
+            })
             this.resultData.length = 0
             this.onload = false
+            if(!this.onload && this.onRepeat) this.start()
         },
 
-        animate(className) {
-            const slotMachineBox = document.querySelector('.slotMachine-reels')
+        animate(className, winBet, boolean) {
+            const slotMachineBox = document.getElementById('slotMachine')
             slotMachineBox.classList.add(className)
-            this.$refs.winline.showLine(className)
+            this.$refs.winline.showLine(className, winBet)
             setTimeout(() => {
                 slotMachineBox.classList.remove(className)
-                this.$refs.winline.hideLine()
-                this.end()
-            }, 3000)
+                this.$refs.winline.hideLine(className, boolean)
+                if (boolean) this.end()
+            }, 1000 * this.winLineTime)
         }
     }
 })
@@ -217,14 +270,16 @@ Vue.component('slot-reel', {
             if (num < 0) num += maxLength
             return num
         },
-        run(maxTurns, onSpeedUp) {
-            const reelTileHeight = document.querySelector('.slotMachine-reels').clientHeight / 3
+        run(maxTurns) {
+            const reelTileHeight = document.getElementById('slotMachine').clientHeight / 3
             let turns = 0, speed = 0
             setTimeout(() => {
                 const runReel = setInterval(() => {
-                    if ((this.reelTileTop + speed + 0.1) < reelTileHeight) {
-                        speed = speed < this.maxSpeed ? speed + 0.1 : this.maxSpeed
-                        if (onSpeedUp) return this.reelTileTop += this.maxSpeed
+                    if (turns == maxTurns - 1 && speed > 5 && this.reelTileTop < reelTileHeight) {
+                        speed--
+                        this.reelTileTop += speed
+                    } else if ((this.reelTileTop + speed + 1) < reelTileHeight) {
+                        speed = speed < this.maxSpeed ? speed + 1 : this.maxSpeed
                         this.reelTileTop += speed
                     } else {
                         this.reelTileTop = 0
@@ -245,7 +300,7 @@ Vue.component('slot-reel', {
             }, 200 * this.value)
         }
     },
-    template: `<div :style="'margin-top:' + reelTileTop + 'px'" class="reel w-1/3">
+    template: `<div :style="'top:' + reelTileTop + 'px'" class="reel relative w-1/3">
         <img class="reel-image mx-auto" :src="reelTileData[getTileIndex(reelTileIndex - 2)].image">
         <img :id="'tile' + value" class="reel-image mx-auto" :src="reelTileData[getTileIndex(reelTileIndex - 1)].image">
         <img :id="'tile' + (value + 3)" class="reel-image mx-auto" :src="reelTileData[getTileIndex(reelTileIndex)].image">
@@ -263,36 +318,51 @@ Vue.component('slot-line', {
             line789: false,
             line159: false,
             line357: false,
+            lineTop: 'top-1/3',
+            lineBet: 0
         }
     },
     beforeMount() {
     },
+    watch: {
+        line123() {
+            if (this.line123) this.lineTop = 'top-0'
+            else this.lineTop = 'top-1/3'
+        },
+        line789() {
+            if (this.line789) this.lineTop = 'bottom-0'
+            else this.lineTop = 'top-1/3'
+        },
+    },
     mounted() {
     },
     methods: {
-        showLine(className) {
-            this[className] = !this[className]
+        showLine(className, winBet) {
+            this[className] = true
+            this.lineBet = winBet
         },
-        hideLine() {
-            Object.assign(this.$data, this.$options.data());
+        hideLine(className, boolean) {
+            this[className] = false
+            if (boolean) this.lineBet = 0
         }
     },
-    template: `<div style="z-index: -1;" class="absolute inset-0">
-        <svg v-if="line123" xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 w-full h-1/3" viewBox="0 0 220 50" fill="none" stroke="#fff" stroke-width="2">
+    template: `<div class="absolute inset-0">
+        <svg v-if="line123" xmlns="http://www.w3.org/2000/svg" style="z-index: -1;" class="absolute top-0 left-0 w-full h-1/3" viewBox="0 0 220 50" fill="none" stroke="#fff" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M0 25 H220" />
         </svg>
-        <svg v-if="line456" xmlns="http://www.w3.org/2000/svg" class="absolute top-1/3 left-0 w-full h-1/3" viewBox="0 0 220 50" fill="none" stroke="#fff" stroke-width="2">
+        <svg v-if="line456" xmlns="http://www.w3.org/2000/svg" style="z-index: -1;" class="absolute top-1/3 left-0 w-full h-1/3" viewBox="0 0 220 50" fill="none" stroke="#fff" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M0 25 H220" />
         </svg>
-        <svg v-if="line789" xmlns="http://www.w3.org/2000/svg" class="absolute bottom-0 left-0 w-full h-1/3" viewBox="0 0 220 50" fill="none" stroke="#fff" stroke-width="2">
+        <svg v-if="line789" xmlns="http://www.w3.org/2000/svg" style="z-index: -1;" class="absolute bottom-0 left-0 w-full h-1/3" viewBox="0 0 220 50" fill="none" stroke="#fff" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M0 25 H220" />
         </svg>
-        <svg v-if="line159" xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 w-full h-full" viewBox="0 0 220 150" fill="none" stroke="#fff" stroke-width="2">
+        <svg v-if="line159" xmlns="http://www.w3.org/2000/svg" style="z-index: -1;" class="absolute top-0 left-0 w-full h-full" viewBox="0 0 220 150" fill="none" stroke="#fff" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M0 25 H35 L185 125 H220" />
         </svg>
-        <svg v-if="line357" xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0 w-full h-full" viewBox="0 0 220 150" fill="none" stroke="#fff" stroke-width="2">
+        <svg v-if="line357" xmlns="http://www.w3.org/2000/svg" style="z-index: -1;" class="absolute top-0 left-0 w-full h-full" viewBox="0 0 220 150" fill="none" stroke="#fff" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M0 125 H35 L185 25 H220" />
         </svg>
+        <p v-if="lineBet > 0" class="lineBet flex justify-center items-center absolute left-0 w-full h-1/3 z-10" :class="lineTop">{{lineBet}}</p>
     </div>`
 })
 
