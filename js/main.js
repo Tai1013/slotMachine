9f1c30a426ddc07a0a6a0f72dabfcf0e1bc2cf4c
Tai Fu <frank22199215@gmail.com>
2022-05-06 18:22:44 +0800

    test orientation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
+    if (window.orientation === 180 || window.orientation === 0) {
+        alert('目前您的螢幕為縱向！');
+    }
+    if (window.orientation === 90 || window.orientation === -90 ){
+        alert('目前您的螢幕為橫向！');
+    }
+}, false);
+
 const app = new Vue({
     data() {
         return {
